0c946f446dc354dfd2e3b3d4c4e81f2033f5b0bf
millerbermeo <millerbermeo30@gmail.com>
2024-02-17 22:05:36 -0500

    commit funciona salida residuos

--- Backend/src/controllers/residuo.controller.js
+++ Backend/src/controllers/residuo.controller.js
@@ -109,3 +109,62 @@ export const registrarResiduo = async (req, res) => {
     }
 };
 
+
+export const registrarSalida = async (req, res) => {
+    try {
+
+        const rol = req.user.rol;
+
+        const id_residuo = req.params.id
+
+        // Validar autorización del usuario
+        if (rol !== 'administrador') {
+            return res.status(HTTP_STATUS.unauthorized).json({ 'message': ERROR_MESSAGE.unauthorized });
+        }
+        
+        //variables del body
+        const { destino, usuario_adm } = req.body;
+
+
+        // if (!Number.isInteger(id_residuo) || !Number.isInteger(cantidad) || !Number.isInteger(usuario_adm) || !Number.isInteger(fk_actividad)) {
+        //     return res.status(HTTP_STATUS.badRequest).json({ 'message': 'id_residuo, cantidad, usuario_adm y fk_actividad deben ser números enteros' });
+        // }        
+
+
+        // Iniciar transacción
+        await pool.query('START TRANSACTION');
+
+        let query = `SELECT * from residuos WHERE id_residuo = ?`;
+
+        // Ejecutar la consulta SQL
+
+        let [result] = await pool.query(query, id_residuo);
+        let almacenamiento = result[0].fk_alm;
+        let cantidad = result[0].cantidad;
+
+        console.log(almacenamiento)
+        console.log(cantidad)
+
+        let query_1 = `UPDATE residuos SET cantidad = '0' WHERE id_residuo = ?`
+        await pool.query(query_1, [id_residuo]);
+
+
+        let query_3 = `UPDATE almacenamiento SET cantidad_alm = '0' WHERE id_almacenamiento = ?`
+        await pool.query(query_3, [almacenamiento]);
+
+
+        let query_2 = `INSERT INTO movimientos (tipo_movimiento, cantidad, fecha, usuario_adm, fk_residuo, fk_actividad) VALUES ('salida', ?, CURDATE(), ?, ?, ?)`;
+
+        // Ejecutar la 2da consulta SQL
+        await pool.query(query_2, [cantidad, usuario_adm, id_residuo, destino]);
+
+        // Confirmar transacción
+        await pool.query('COMMIT');
+
+        return res.status(HTTP_STATUS.ok).json({ 'message': 'Movimiento registrado correctamente' });
+    } catch (error) {
+        // Manejo de errores
+        console.error('Error en registrarMovimiento:', error);
+        return res.status(HTTP_STATUS.internalServerError).json({ 'message': ERROR_MESSAGE.internalServerError });
+    }
+};
--- Backend/src/routers/residuo.router.js
+++ Backend/src/routers/residuo.router.js
@@ -2,11 +2,12 @@
 
 import { Router } from "express";
 import { validarToken } from "../controllers/validator.controller.js";
-import { registrarMovimiento, registrarResiduo } from "../controllers/residuo.controller.js";
+import { registrarMovimiento, registrarResiduo, registrarSalida } from "../controllers/residuo.controller.js";
 
 const router = Router()
 
 router.post('/registrar', validarToken, registrarResiduo)
 router.post('/registrarmov', validarToken, registrarMovimiento)
+router.post('/registrarsal/:id', validarToken, registrarSalida)
 
 export default router
