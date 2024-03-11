e144e00e92c547799da0d27c2480133a7dd922e3
Camilo3008 <camilo.alvarez3008@gmail.com>
2024-03-11 07:38:10 -0500

    actividad

--- Backend/src/controllers/actividad.controller.js
+++ Backend/src/controllers/actividad.controller.js
@@ -1,1 +1,29 @@
-//CONTROLADOR JOSE
+import { pool } from "../database/conexion.js";
+
+export const agregarActividad = async (req, res) => {
+    try {
+        const rol = req.user.rol;
+
+        // usuario tiene el rol de administrador
+        if (rol === 'administrador') {
+            const { id_tipo_actividad, nombre_actividad, descripcion_actividad } = req.body;
+
+            // datos necesarios estan presentes
+            if (!id_tipo_actividad || !nombre_actividad || !descripcion_actividad) {
+                return res.status(400).json({ 'message': 'Faltan datos obligatorios para la actividad' });
+            }
+
+            // Insertar la nueva actividad en la base de datos
+            const result = await pool.query(
+                "INSERT INTO tipos_actividades SET ?",
+                [id_tipo_actividad, nombre_actividad, descripcion_actividad]
+            );
+// Devolver la actividad recién creada
+            return res.status(201).json(result.rows[0]); 
+        } else {
+            return res.status(403).json({ 'message': 'Error: usuario no autorizado' });
+        }
+    } catch (e) {
+        return res.status(500).json({ 'message': 'Error: ' + e });
+    }
+};
--- Backend/src/routers/actividad.router.js
+++ Backend/src/routers/actividad.router.js
@@ -1,1 +1,10 @@
-//RUTAS JOSE
+
+import express from "express";
+import {agregarActividad} from "../controllers/actividad.controller.js"; 
+
+const router = express.Router();
+
+// Agregar una nueva actividad
+router.post("/actividades", agregarActividad);
+
+export default router;
